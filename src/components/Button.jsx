768080e1dafbd62f17da108ce95492534d42a799
raulDeDevWorld <proyection009@gmail.com>
2023-12-03 14:10:45 -0400

    DC

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -30,7 +30,7 @@ export default function Button({ theme, styled, click, children }) {
         case 'Primary':
             return <button
                 type="submit"
-                className="text-white bg-[#422C39] border border-gray-100 hover:bg-gray-950 focus:ring-4 focus:outline-none focus:ring-gray-950 font-bold rounded-xl text-[16px] text-white font-bold w-full  px-3 py-3 my-2 text-center transition ease-in-out"
+                className="text-white bg-green-400 border border-gray-100 hover:bg-gray-950 focus:ring-4 focus:outline-none focus:ring-gray-950 font-bold rounded-xl text-[16px] text-white font-bold w-full  px-3 py-3 my-2 text-center transition ease-in-out"
                 // className="text-white bg-violet-700 hover:bg-violet-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center 
                 // dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 onClick={click}
